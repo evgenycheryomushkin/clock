ddf33c3ea9e0b42ef3f0e5015e297e6d9e03cc64
Evgeny Cheryomushkin <evgeny.cheryomushkin@gmail.com>
2024-03-03 21:46:21 +0700

    fix routes error

--- app/src/app/routes.ts
+++ app/src/app/routes.ts
@@ -5,28 +5,29 @@ import { AppComponent } from "./app.component";
  * send verification event to backend.
  * It is verified on backend and event is sent back.
  * If route is ok then user is allowed to work with UI, otherwise
- * not. So route is important to keept safe to access your tasks.
- * 
+ * not. So route is important to keep safe to access your tasks.
+ *
  * If route is new backend generates route.
  */
 export const routes: Routes = [
     {
       matcher: (url) => {
         console.log("url", url)
-        if (url.length === 1 
-          && url[1].path.match(RegExp("^([0-9abcdef]{8}|)$","i"))) {
-            const key = url[0].path
-          console.log("key", key)
-          return {
-            consumed: url,
-            posParams: {
-              key: new UrlSegment(key, {})
-            }
-          };
+        if (url.length === 1) {
+          const path = url[0].path;
+          if (path.match(RegExp("^([0-9abcdef]{8}|)$","i"))) {
+              const key = url[0].path
+            console.log("key", key)
+            return {
+              consumed: url,
+              posParams: {
+                key: new UrlSegment(key, {})
+              }
+            };
+          }
         }
         return null;
       },
       component: AppComponent
     }
-  ]; 
-  
+  ];
